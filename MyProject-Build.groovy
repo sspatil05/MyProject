57b361bf8f304ac7686bf8925258fd77e870564c
sspatil05 <46047052+sspatil05@users.noreply.github.com>
2019-08-24 20:32:43 +0530

    Update MyProject-Build.groovy

--- MyProject-Build.groovy
+++ MyProject-Build.groovy
@@ -1,25 +1,145 @@
-// MyProject-Build.groovy
+branch = "master"
 
-def gitUrl = "https://github.com/example/project.git"
+git_url = "https://github.deere.com/parts/parts-hcs-infra"
 
-job("MyProject-Build") {
-    description "Builds MyProject from master branch."
-    parameters {
-        stringParam('COMMIT', 'HEAD', 'Commit to build')
-    }
-    scm {
+ 
+
+freeStyleJob("sachin-test") {
+
+    description 'Build job for Hybris'
+
+    logRotator(daysToKeep = -1, numToKeep = 15, artifactDaysToKeep = -1, artifactNumToKeep = -1)
+
+    label('devl-slave')
+
+ 
+
+          scm {
+
         git {
+
             remote {
-                url gitUrl.
-                branch "origin/master"
+
+                url("${git_url}")
+
+                credentials('hcs-infra-github-token')
+
             }
+
+            branch("${branch}")
+
             extensions {
-                wipeOutWorkspace()
-                localBranch master
+
             }
+
         }
+
+ 
+
     }
+
+    parameters {
+
+            choiceParam("HYBRIS_COMMERCE_SUITE", ["HYBRISCOMM6300P_7-70002554.zip","HYBRISCXCOMM181100P_5-70004085.zip","HYBRISCOMM6500P_3-80003045.zip"])
+
+            stringParam('HYBRIS_OPT_CONFIG_DIR','${WORKSPACE}/hybris/env/ci')
+
+                   choiceParam("branch", ["hybrislighty","hybrisaws"])
+
+                  
+
+    }
+
+ 
+
+    wrappers {
+
+      preBuildCleanup()
+
+      maskPasswords()
+
+     
+
+    }
+
     steps {
-        shell "Look: I'm building master!"
+
+        shell('''
+
+ant -version
+
+             ''')
+
     }
+
+ 
+
+    steps {
+
+        ant {
+
+            
+
+            targets(['install'])
+
+            props('environment': 'ci','hybris.zip.package.src': '../hcs_57_hybris_install_media/${HYBRIS_COMMERCE_SUITE}')       
+
+            antInstallation('Ant 1.9')
+
+           
+
+        }
+
+        ant {                             
+
+            targets(['clean', 'customize', 'all', 'production'])
+
+            props('environment': 'ci','hybris.zip.package.src': '../hcs_57_hybris_install_media/${HYBRIS_COMMERCE_SUITE}')       
+
+            
+
+        }
+
+    }
+
+ 
+
+ 
+
+    steps {
+
+        shell('''
+
+zip -j deere-hybris.zip hybris/temp/hybris/hybrisServer/*.zip
+
+cd hybris/temp/hybris/hybrisServer/
+
+unzip hybrisServer-solr.zip
+
+ 
+
+#moving the custom configrations from "hybris/config/solr/configsets/"
+
+#cp -rf /var/lib/jenkins/workspace/hcs_57/hcs_57_build/hybris/config/solr/configsets/ /var/lib/jenkins/workspace/hcs_57/hcs_57_build/hybris/bin/ext-commerce/solrserver/resources/solr/server/solr/
+
+ 
+
+cp -rf /var/lib/jenkins/workspace/hcs_57/hcs_57_build/hybris/config/solr/configsets/ /var/lib/jenkins/workspace/hcs_57/hcs_57_build/hybris/temp/hybris/hybrisServer/hybris/bin/ext-commerce/solrserver/resources/solr/server/solr/
+
+mv hybris/bin/ext-commerce/solrserver/resources/solr .
+
+ 
+
+#rm -r hybris
+
+tar -czvf deere-solr.tgz solr
+
+#rm -r solr
+
+             ''')
+
+    }
+
+ 
+
 }
